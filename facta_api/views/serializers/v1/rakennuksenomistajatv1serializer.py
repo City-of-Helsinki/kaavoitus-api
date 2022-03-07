1efd9955e6e4c5316aeffa1c2c3dbec32df1d376
Lauri H <lauri.haukijarvi@nidos.fi>
2022-03-07 20:40:49 +0200

    More permissive "RakennuksenOmistajat" serializer

--- facta_api/views/serializers/v1/rakennuksenomistajatv1serializer.py
+++ facta_api/views/serializers/v1/rakennuksenomistajatv1serializer.py
@@ -4,4 +4,4 @@ from .rakennuksenomistajav1serializer import RakennuksenOmistajaV1Serializer
 
 class RakennuksenOmistajatV1Serializer(serializers.Serializer):
     kiinteistotunnus = serializers.CharField(max_length=17)
-    omistajat = RakennuksenOmistajaV1Serializer(many=True)
+    omistajat = RakennuksenOmistajaV1Serializer(many=True,required=False)
