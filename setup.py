40effaadc8c46f7c1af11315a793884e0da0c708
HenryVisuri <henry.visuri@haallas.com>
2022-12-08 16:12:03 +0200

    Removed redundant django-redis from install_requires as proper version is installed with django-sentinel.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "GDAL >=3.0.0,<3.1",
         "sentry-sdk == 1.5.6",
         "elastic-apm == 6.8.1",
-        "django-redis == 5.2.0",
         "django-sentinel == 0.1.0",
     ],
     extras_require={
