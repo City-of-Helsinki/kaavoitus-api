eb48554b595588aae39161b6a9c731ae5b48986b
HenryVisuri <henry.visuri@haallas.com>
2022-12-21 15:51:44 +0200

    Added kiinteistotunnukset view for getting a list of property codes by hankenumero

---
+++ geoserver_api/views/v1/kiinteistotunnukset.py
@@ -0,0 +1,54 @@
+from rest_framework.views import APIView  # pip install django-rest-framework
+from django.http import (
+    JsonResponse,
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseNotFound,
+    HttpResponseForbidden,
+)
+import logging
+from geoserver_api import hki_geoserver
+from api_project.utils import format_kiinteistotunnus
+
+log = logging.getLogger(__name__)
+
+
+class API(APIView):
+
+    def get(self, request, hankenumero=None):
+        if not hankenumero:
+            return HttpResponseBadRequest("Need hankenumero!")
+        if not request.auth:
+            return HttpResponse(status=401)
+        geoserver_creds = request.auth.access_geoserver
+        if not geoserver_creds:
+            return HttpResponseForbidden("No access!")
+
+        # Confirmed access to GeoServer.
+        # Go get the data!
+        kh = hki_geoserver.Kaavahanke(
+            username=geoserver_creds.username,
+            password=geoserver_creds.credential,
+        )
+        data = kh.get_by_hankenumero(hankenumero)
+
+        if not data:
+            return HttpResponseNotFound("No data found for hankenumero!")
+
+        kt = hki_geoserver.Kiinteistotunnus(
+            username=geoserver_creds.username,
+            password=geoserver_creds.credential
+        )
+        kt_data = kt.get_by_geom(data)
+
+        if not kt_data:
+            return HttpResponseNotFound("No kiinteisto data found by geometry")
+
+        kiinteistot = []
+
+        for kiinteisto in kt_data:
+            kiinteistotunnus = format_kiinteistotunnus(kiinteisto["kiinteistotunnus"])
+            if kiinteistotunnus:
+                kiinteistot.append(kiinteistotunnus)
+
+        return JsonResponse({"kiinteistotunnukset": kiinteistot})
