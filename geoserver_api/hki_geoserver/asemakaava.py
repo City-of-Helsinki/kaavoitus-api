21492ca77f56bef485a2ee9e51f774f6bdedb3f7
HenryVisuri <henry.visuri@haallas.com>
2025-02-18 10:19:25 +0200

    Change asemakaava layer from helsinki:asemakaava to hel:asemakaava

--- geoserver_api/hki_geoserver/asemakaava.py
+++ geoserver_api/hki_geoserver/asemakaava.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 
 class Asemakaava(GeoServer_Reader):
     use_auth = False
-    layername = "helsinki:asemakaava"
+    layername = "hel:asemakaava"
     use_old_url = True
     schema = {
         "geometry": "GeometryCollection",
