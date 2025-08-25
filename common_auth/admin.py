83e79a212435dfe8df3cd5798bd68e40b0f5b33a
HenryVisuri <henry.visuri@haallas.com>
2025-08-25 13:22:57 +0300

    Django admin changes

--- common_auth/admin.py
+++ common_auth/admin.py
@@ -1,4 +1,20 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models.token_auth import Token
+from .models.ext_auth_cred import ExtAuthCred
 
 
-# Register your models here.
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created', 'access_facta', 'access_geoserver', 'access_kaavapino')
+    list_filter = ('created', 'access_facta', 'access_geoserver', 'access_kaavapino')
+    search_fields = ('user__username', 'user__email', 'key')
+    readonly_fields = ('key', 'created')
+    raw_id_fields = ('user', 'access_facta', 'access_geoserver', 'access_kaavapino')
+
+
+@admin.register(ExtAuthCred)
+class ExtAuthCredAdmin(admin.ModelAdmin):
+    list_display = ('id', 'system', 'cred_owner', 'username', 'host_spec')
+    list_filter = ('system', 'cred_owner')
+    search_fields = ('system', 'cred_owner', 'username', 'host_spec')
+    fields = ('system', 'cred_owner', 'username', 'credential', 'host_spec')
