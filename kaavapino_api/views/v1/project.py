fa4d29b60499117b84cba5b2c4fad2c1b4ed08be
HenryVisuri <henry.visuri@haallas.com>
2026-06-02 12:25:36 +0300

    Changed serializer error log level from info to error

--- kaavapino_api/views/v1/project.py
+++ kaavapino_api/views/v1/project.py
@@ -51,7 +51,7 @@ class API(APIView):
         # It needs to be verifiable by serializer rules. Those are published in Swagger.
         serializer = self.serializer_class(data=data)
         if not serializer.is_valid():
-            log.info(f'Serializer received invalid data: {serializer.errors}')
+            log.error(f'Project data serialization failed, errors: {serializer.errors}')
             return HttpResponseServerError("Invalid data received from Kaavapino")
 
         return JsonResponse(serializer.validated_data)
